562ee7b06a1d081bb5de71ad9ffd526eaac0dc0d
gominzip <ahaminji@naver.com>
2023-11-12 01:43:14 +0900

    [CHORE] 불필요한 코드 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,14 @@ import Footer from '@/components/common/Footer';
 import Header from '@/components/common/Header';
 import NavBar from '@/components/common/NavBar';
 import LandingHome from '@/components/landing/LandingHome';
-import Head from 'next/head';
 
 export default function Page() {
 	return (
 		<div>
-			<Head>
-				<title>My page title</title>
-				<meta property="og:title" content="My page title" key="title" />
-			</Head>
 			<Header where="main" />
-			<NavBar />
-			<LandingHome />
-			<Footer />
+			<NavBar/>
+			<LandingHome/>
+			<Footer/>
 		</div>
 	);
 }
